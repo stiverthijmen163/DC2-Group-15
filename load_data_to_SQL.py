4a4cf223fd11b0a0239d7158120860a3992312a9
stiverthijmen163 <t.m.a.broeren@student.tue.nl>
2024-05-03 11:50:02 +0200

    update db

--- load_data_to_SQL.py
+++ load_data_to_SQL.py
@@ -2,6 +2,7 @@ import pandas as pd
 import sqlite3
 import os, sys
 import openpyxl  # Needed to open xlsx files <DO NOT REMOVE>
+import numpy as np
 
 
 def load_data():
@@ -19,6 +20,13 @@ def load_data():
     # All filenames containing 'street'
     table_name = "street"
     df = get_data(data_path, "street")
+
+    # clean the data
+    df = lower_case_data(df)
+    df["year"] = df["month"].astype(str).str[:4]
+    df = df[df["year"].astype(int) >= 2014]
+    df = df.drop(columns=["year"])
+
     df.to_sql(table_name, conn, index=False, if_exists="replace")
     conn.commit()
     print(f"CSV data successfully imported into SQLite database: {SQL_path}, table: {table_name}")
@@ -26,6 +34,13 @@ def load_data():
     # All filenames containing 'outcomes'
     table_name = "outcomes"
     df = get_data(data_path, "outcomes")
+
+    # clean the data
+    df = lower_case_data(df)
+    df["year"] = df["month"].astype(str).str[:4]
+    df = df[df["year"].astype(int) >= 2014]
+    df = df.drop(columns=["year"])
+
     df.to_sql(table_name, conn, index=False, if_exists="replace")
     conn.commit()
     print(f"CSV data successfully imported into SQLite database: {SQL_path}, table: {table_name}")
@@ -33,6 +48,12 @@ def load_data():
     # All filenames containing 'stop'
     table_name = "stop_and_search"
     df = get_data(data_path, "stop")
+
+    # clean the data
+    df = lower_case_data(df)
+    df["month"] = df["date"].astype(str).str[:7]
+    df = df.drop(columns=["date"])
+
     df.to_sql(table_name, conn, index=False, if_exists="replace")
     conn.commit()
     print(f"CSV data successfully imported into SQLite database: {SQL_path}, table: {table_name}")
@@ -43,6 +64,12 @@ def load_data():
     # MPS table within PAS csv file
     table_name = "PAS_MPS"
     df = pd.read_excel(path, sheet_name="MPS")
+
+    # clean the data
+    df = lower_case_data(df)
+    df["month"] = df["date"].astype(str).str[:7]
+    df = df.drop(columns=["date"])
+
     df.to_sql(table_name, conn, index=False, if_exists="replace")
     conn.commit()
     print(f"CSV data successfully imported into SQLite database: {SQL_path}, table: {table_name}")
@@ -53,6 +80,12 @@ def load_data():
 
     # get the correct columns
     df = df[df.columns[:6]].copy()
+
+    # clean the data
+    df = lower_case_data(df)
+    df["month"] = df["date"].astype(str).str[:7]
+    df = df.drop(columns=["date"])
+
     df.to_sql(table_name, conn, index=False, if_exists="replace")
     conn.commit()
     print(f"CSV data successfully imported into SQLite database: {SQL_path}, table: {table_name}")
@@ -76,7 +109,6 @@ def get_data(path: str, word: str) -> pd.DataFrame:
     for file in dir:
         if word in file:
             df0 = pd.read_csv(f"{path}/{file}")
-            print(f"{path}/{file}")
 
             # create one large dataframe
             if df is None:
@@ -84,6 +116,16 @@ def get_data(path: str, word: str) -> pd.DataFrame:
             else:
                 df = pd.concat([df, df0])
 
+    return df
+
+
+def lower_case_data(df: pd.DataFrame) -> pd.DataFrame:
+    columns = [i.replace(" ", "_").lower() for i in df.columns.to_list()]
+    df = df.set_axis(columns, axis=1)
+
+    for column in columns:
+        if df[column].dtype == object:
+            df[column] = df[column].astype(str).str.lower()
     return df
 
 
