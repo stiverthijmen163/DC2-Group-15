7c29ac5b4671e2a47e0950257142b47b6c031f7d
20224113 <a.langenhuizen@student.tue.nl>
2024-05-08 11:17:53 +0200

    Added regression tree types

--- feature_selection.py
+++ feature_selection.py
@@ -1,15 +1,22 @@
 import sqlite3
 import pandas as pd
-from sklearn.datasets import make_regression
 from sklearn.tree import DecisionTreeRegressor
-from matplotlib import pyplot
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+import matplotlib.pyplot as plt
 
+# Connect to db
+cnx = sqlite3.connect('data/police_data.db')
 
-cnx = sqlite3.connect('data/data/police_data.db')
-df_PAS_Borough = pd.read_sql_query("SELECT * FROM PAS_Borough GROUP BY Date, Borough, Measure", cnx)
+# Group by measure since the trust is measured per measure
+df_PAS_Borough = pd.read_sql_query("SELECT * FROM PAS_Borough", cnx)
+df_stop_and_search = pd.read_sql_query("SELECT * FROM stop_and_search GROUP BY month, borough", cnx)
 
-# Inconsistency in the data: some rows have upon without capital U, others do
-df_PAS_Borough['Borough'] = df_PAS_Borough['Borough'].replace('Richmond Upon Thames', 'Richmond upon Thames')
+df_merged = pd.merge(df_PAS_Borough, df_stop_and_search, on=['borough', 'month'], how='inner')
+df_merged = df_merged.drop(['survey', 'latitude', 'longitude', 'part_of_a_policing_operation',
+                            'policing_operation', 'outcome_linked_to_object_of_search',
+                            'removal_of_more_than_just_outer_clothing'], axis=1)
+
 
 # Feature analysis with Decision tree. NOTE: So far we can only use one variable, since other variables are strings
 # To solve the string values we need to consider making dummy variables. For example: every crime types gets its own
@@ -17,25 +24,94 @@ df_PAS_Borough['Borough'] = df_PAS_Borough['Borough'].replace('Richmond Upon Tha
 # Source: https://machinelearningmastery.com/calculate-feature-importance-with-python/
 
 # Converting the Boroughs to dummy variables
-df_PAS_Borough = pd.get_dummies(df_PAS_Borough, columns=['Borough'])
+df_merged = df_merged = pd.get_dummies(df_merged, columns=['type', 'gender', 'age_range',
+                                                           'officer_defined_ethnicity', 'object_of_search', 'outcome'])
 
-print(df_PAS_Borough.iloc[:, 5:-1].columns)
-
-# Define X (the values we use to predict y) and define Y (= trust level)
-X, y = df_PAS_Borough.iloc[:, 5:-1], df_PAS_Borough['MPS']
+# Define X (the values we use to predict y) and define y (= trust level)
+X, y = df_merged.iloc[:, 7:-1], df_merged['mps']
 
 # Create and fit the model
-model = DecisionTreeRegressor()
+model = DecisionTreeRegressor()   # Using random_state so that we can reproduce results, since the features are always randomly permuted at each split
 model.fit(X, y)
 
 # get importance
-importance = model.feature_importances_
+importances = pd.DataFrame(data={
+    'attribute': X.columns,
+    'importance': model.feature_importances_
+})
+importances = importances.sort_values(by='importance', ascending=False)
 
-# summarize feature importance
-for i in range(len(importance)):
-    print(f'Feature {i+1}: {df_PAS_Borough.iloc[:, 5:-1].columns[i]}, Score: {importance[i]}')
+print('Feature importance values using Decision Tree Regressor in descending order:')
+for index, row in importances.iterrows():
+    attribute = row['attribute']
+    score = row['importance']
+    print(f'Attribute Name: {attribute}, Score: {score}')
+
 
 # plot feature importance
-pyplot.bar([x for x in range(len(importance))], importance)
-pyplot.show()
+plt.bar(x=importances['attribute'], height=importances['importance'])
+plt.title("Feature importance using Decision Tree Regressor")
+plt.xticks(rotation='vertical', size=5)
+plt.show()
 
+# Feature analysis with Random Forest Regression
+model = RandomForestRegressor()
+
+# fit the model
+model.fit(X, y)
+
+# get importance
+importances = pd.DataFrame(data={
+    'attribute': X.columns,
+    'importance': model.feature_importances_
+})
+importances = importances.sort_values(by='importance', ascending=False)
+
+# summarize feature importance
+print('\n')
+print('Feature importance values using Random Forest Regression in descending order:')
+for index, row in importances.iterrows():
+    attribute = row['attribute']
+    score = row['importance']
+    print(f'Attribute Name: {attribute}, Score: {score}')
+
+# plot feature importance
+plt.bar(x=importances['attribute'], height=importances['importance'])
+plt.title("Feature importance using Random Forest Regression")
+plt.xticks(rotation='vertical', size=5)
+plt.show()
+
+# Feature analysis with XGBoost (Extreme Gradient Boosting)
+# XGBoost is based on the concept of gradient boosting, which is an ensemble technique where multiple weak learners
+# (typically decision trees) are combined to create a strong learner. Gradient boosting builds trees sequentially,
+# where each new tree tries to correct the errors made by the previous ones.
+# documnetation link: https://xgboost.readthedocs.io/en/latest/index.html
+
+# create model
+model = XGBRegressor()
+
+# fit the model
+model.fit(X, y)
+
+# get importance
+importances = pd.DataFrame(data={
+    'attribute': X.columns,
+    'importance': model.feature_importances_
+})
+importances = importances.sort_values(by='importance', ascending=False)
+
+# summarize feature importance
+print('\n')
+print('Feature importance values using XGBoost in descending order:')
+for index, row in importances.iterrows():
+    attribute = row['attribute']
+    score = row['importance']
+    print(f'Attribute Name: {attribute}, Score: {score}')
+
+# plot feature importance
+plt.bar(x=importances['attribute'], height=importances['importance'])
+plt.title("Feature importance using XGBoost")
+plt.xticks(rotation='vertical', size=5)
+plt.show()
+
+# Things we still need to take into account: time lag!
